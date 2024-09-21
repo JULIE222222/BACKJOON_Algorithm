64d826c93a7f8ef7c40fcf4fb25e3e08da73ccea
JIYUN <167040360+JULIE222222@users.noreply.github.com>
2024-09-21 15:26:47 +0900

    [Bronze V] Title: 꼬마 정민, Time: 164 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);	
+		Long num1 = sc.nextLong();
+		Long num2 = sc.nextLong();
+		Long num3 = sc.nextLong();
+	
+		System.out.println(num1 + num2 + num3);
+				
+	}
+
+}
