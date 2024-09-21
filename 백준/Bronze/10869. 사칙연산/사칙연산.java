a7bedd46c51eb363255ff1a25876844a4c91df17
JIYUN <167040360+JULIE222222@users.noreply.github.com>
2024-09-21 14:54:22 +0900

    [Bronze V] Title: 사칙연산, Time: 164 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		long num1 = sc.nextLong();
+		long num2 = sc.nextLong();
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num2);
+		
+	}
+
+}
